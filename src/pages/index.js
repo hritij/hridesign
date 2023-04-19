0e014c35309d416e6ebb728d2c09f0da1aa3ee06
Subhajit Nandi <subhajit.nandi@innovaccer.com>
2023-04-19 13:40:57 +0530

    feat: adds meeting form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,33 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
 
-import logo from "../assets/hri_white.jpg";
+import logo from '../assets/hri_white.jpg'
+import BookMeetingForm from '@/components/forms/bookMeeting'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <nav className="flex justify-around">
-      <Image src={logo} width={100} />
-      <div className="w-1/2">
-        <div className="flex justify-around flex-grow">
-          <div>Design gallery</div>
-          <div>Services</div>
-          <div>About Us</div>
-        </div>
-      </div>
-      <div>
-        <button className="px-2 py-3 bg-red-400 text-white rounded-md hover:bg-red-600">
-          Get Free Quote
-        </button>
-      </div>
-    </nav>
-  );
+	return (
+		<>
+			<nav className="flex justify-around items-center">
+				<Image src={logo} width={100} />
+				<div className="w-1/2 hidden md:block">
+					<div className="flex justify-around flex-grow">
+						<div>Design gallery</div>
+						<div>Services</div>
+						<div>About Us</div>
+					</div>
+				</div>
+				<div>
+					<Link href="/quote">
+						<button className="px-3 py-2 bg-red-400 text-white rounded-md hover:bg-red-600">Get Free Quote</button>
+					</Link>
+				</div>
+			</nav>
+			<BookMeetingForm />
+		</>
+	)
 }
---
+++ src/pages/quote.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function QuoteForm() {
+	return (
+		<div>
+			<div className="text-lg font-bold">LET'S GET STARTED</div>
+			<div className=" text-xs font-normal">STEP 1 OF 3</div>
+			<div>
+				<div>
+					<div>Your floorplan</div>
+				</div>
+			</div>
+		</div>
+	)
+}
